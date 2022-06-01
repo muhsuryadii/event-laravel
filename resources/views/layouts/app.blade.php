97015492a1f8c11ca717980ddc78d8ab4d05e165
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2022-06-01 09:19:05 +0700

    feat: add argon template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,8 @@
 
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
+
+    {{-- Temo --}}
 </head>
 
 <body class="font-sans antialiased">
--- resources/views/layouts/head_parts.blade.php
+++ resources/views/layouts/head_parts.blade.php
@@ -6,3 +6,4 @@
         rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="shortcut icon" href={{ asset('image/logo_usni.png') }} type="image/x-icon">
