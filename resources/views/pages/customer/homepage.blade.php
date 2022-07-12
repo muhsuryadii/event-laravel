e3a039a5a35841b8fbee00d54931dcfe8d73786b
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2022-07-12 09:31:44 +0700

    feat : add dompdf

--- resources/views/pages/customer/homepage.blade.php
+++ resources/views/pages/customer/homepage.blade.php
@@ -25,7 +25,7 @@
                                         class="form-control mr-3 relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                         placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
                                     <button
-                                        class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150ss ease-in-out flex items-center"
+                                        class="  px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150ss ease-in-out flex items-center"
                                         type="button" id="button-addon2">
                                         <i class="fa-solid fa-magnifying-glass"></i>
                                     </button>
